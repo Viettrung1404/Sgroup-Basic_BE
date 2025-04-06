4a28c1ba8c06fd4497ba5e827f0adb8ee921743b
Viettrung <nguyenviettrung2005@gmail.com>
2025-04-06 16:34:53 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,85 +1,82 @@
 import express from 'express';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const path = join(__dirname, '../data.json');
 
 const app = express();
 const port = 3000;
 
+let parsedData;
+let users;
+
 app.use(express.json());
 
-const path = '../db.json';
-
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Lalala');
 })
 
-app.get('/users', (req, res) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ message: 'Internal server error' });
-        }
-        res.json(JSON.parse(data).users);
-    });
+app.get('/users', async (req, res) => {
+    try {
+        await readFileData();
+        res.json(users);
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: 'Internal server error' });
-        }
-        const users = JSON.parse(data).users;
-        const user = users.find((u) => u.id === Number(req.params.id))
-        res.json(user)
-    });
+app.get('/:id', async (req, res) => {
+    await readFileData();
+    const user = users.find((u) => u.id === Number(req.params.id));
+    res.json(user);
 })
 
-app.post('/create', (req, res) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: 'Internal server error' });
+app.post('/', async (req, res) => {
+    try {
+        if (!req.body.name) {
+            return res.status(400).json({ message: 'Name is required' });
         }
-        let parsedData = JSON.parse(data);
-        let users = parsedData.users;
-        const newUser = { id: users.length + 1, name: req.body.name };
+        await readFileData();
+        const newUser = { id: users[users.length - 1].id + 1, name: req.body.name };
         users.push(newUser);
 
         parsedData.users = users;
-        fs.writeFile(path, JSON.stringify(parsedData), (err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Internal server error' });
-            }
-        });
+        await writeFileData(parsedData);
         res.status(201).json(users);
-    });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
-app.put('/update/:id', (req, res) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: 'Internal server error' });
+app.put('/:id', async (req, res) => {
+    try {
+        if (!req.body.name) {
+            return res.status(400).json({ message: 'Name is required' });
         }
-        let parsedData = JSON.parse(data);
-        let users = parsedData.users;
-        const user = users.find((u) => u.id === Number(req.params.id));
-        user.name = req.body.name;
+        await readFileData();
+        const { id } = req.params;
+        const index = users.findIndex((u) => u.id === Number(id));
+        if (index === -1) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        users[index].name = req.body.name;
 
         parsedData.users = users;
-        fs.writeFile(path, JSON.stringify(parsedData), (err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Internal server error' });
-            }
-        });
+        await writeFileData(parsedData);
         res.json(users);
-    });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
-app.delete('/delete/:id', (req, res) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ message: 'Internal server error' });
-        }
-        let parsedData = JSON.parse(data);
-        let users = parsedData.users;
+app.delete('/:id', async (req, res) => {
+    try {
+        await readFileData();
         const { id } = req.params;
         const index = users.findIndex((u) => u.id === Number(id));
         if (index === -1) {
@@ -88,24 +85,40 @@ app.delete('/delete/:id', (req, res) => {
         users.splice(index, 1);
 
         parsedData.users = users;
-        fs.writeFile(path, JSON.stringify(parsedData), (err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Internal server error' });
-            }
-        });
+        await writeFileData(parsedData);
         res.json(users);
-    });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
-app.post('/hello',
-    (req, res, next) => {
-        console.log('Hello middleware');
-        next();
-    },
-    (req, res) => {
-        res.send('Hello world');
+async function readFileData() {
+    try {
+        const data = await fs.promises.readFile(path, 'utf8');
+        parsedData = JSON.parse(data);
+        users = parsedData.users;
+    } catch (err) {
+        console.error('Error reading file:', err);
+        throw err;
     }
-)
+}
+
+async function writeFileData(data) {
+    try {
+        await fs.promises.writeFile(path, JSON.stringify(data, null, 2), 'utf8');
+    } catch (err) {
+        console.error('Error writing file:', err);
+        throw new Error('Internal server error');
+    }
+}
+
+function errorHandler(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+}
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
